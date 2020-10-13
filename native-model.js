343d4da6af5a21959cde9201c073b6e23d23ffb4
Tumble <tumblegamer@gmail.com>
2020-10-13 17:07:51 +0100

    ok

---
+++ native-model.js
@@ -0,0 +1,37 @@
+/*
+<div id="BCM_modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            </div>
+        </div>
+	</div>
+
+*/
+
+
+/*
+<div id="BCM_modal" class="modal fade show" tabindex="-1" role="dialog" aria-hidden="false" style="display: block;">
+        <div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header"></div>
+				<div class="modal-body"></div>
+				<div class="modal-footer"></div>
+			<div>
+        </div>
+	</div>
+
+	<div class="modal-backdrop fade show"></div>
+*/
+
+/*
+body.modal-open
+class="show" 
+aria-hidden="false" style="display: block;">
+*/
+
+(function(){
+	"use strict";
+	var backdrop = document.createElement("div");
+	backdrop.classList.add("model-backdrop fade show")
+
+})();
